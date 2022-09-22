95bf26b8778673964ac85e1dc03c05d46cda07a9
bramwell simiyu <simiyub.engineer@gmail.com>
2022-09-22 22:37:17 +0100

    shell project

---
+++ learn-account/src/main/java/tech/topskills/learnaccount/LearnAccountApplication.java
@@ -0,0 +1,13 @@
+package tech.topskills.learnaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAccountApplication.class, args);
+	}
+
+}
---
+++ learn-account/src/test/java/tech/topskills/learnaccount/LearnAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.topskills.learnaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
