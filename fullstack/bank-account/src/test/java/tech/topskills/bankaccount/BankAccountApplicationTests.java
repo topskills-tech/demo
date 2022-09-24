44a6540c96d752ea856cd364f6d3e3044d59fd29
bramwell simiyu <simiyub.engineer@gmail.com>
2022-09-24 05:28:26 +0100

    shell of a new sample project

---
+++ fullstack/bank-account/src/test/java/tech/topskills/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.topskills.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
